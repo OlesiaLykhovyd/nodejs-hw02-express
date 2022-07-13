6b45c9155f0ecc8fbe2023df590233801a7e3f6c
OlesiaLykhovyd <olesyalykhovyd@gmail.com>
2022-07-13 14:32:25 +0200

    added logout, getCurrent

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -14,6 +14,7 @@ const login = async (req, res, next) => {
     }
     const payload = { id: user._id };
     const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" });
+    await User.findByIdAndUpdate(user._id, { token });
     res.json({ token });
   } catch (error) {
     next(error);
