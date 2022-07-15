ac77117033991b1e52874fbd4204017959739d29
OlesiaLykhovyd <olesyalykhovyd@gmail.com>
2022-07-15 17:36:44 +0200

    added avatar, update avatar

---
+++ controllers/auth/updateAvatar.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const fs = require("fs/promises");
+const { User } = require("../../models/user");
+
+const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
+
+const updateAvatar = async (req, res, next) => {
+  try {
+    const { path: tempDir, originalname } = req.file;
+    const { _id } = req.user;
+    const [extention] = originalname.split(".").reverse();
+    const newName = `${_id}.${extention}`;
+    const resultDir = path.join(avatarsDir, newName);
+
+    await fs.rename(tempDir, resultDir);
+
+    const avatarURL = path.join("avatars", newName);
+
+    await User.findByIdAndUpdate(_id, { avatarURL });
+
+    res.json({ avatarURL });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = updateAvatar;
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const controllers = require("../../controllers/auth");
-const { validation, authenticate } = require("../../middlewares");
+const { validation, authenticate, upload } = require("../../middlewares");
 const { schemas } = require("../../models/user");
 
 router.post("/signup", validation(schemas.register), controllers.register);
 router.post("/login", validation(schemas.login), controllers.login);
 router.get("/current", authenticate, controllers.getCurrent);
 router.get("/logout", authenticate, controllers.logout);
+router.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  controllers.updateAvatar
+);
 
 module.exports = router;
